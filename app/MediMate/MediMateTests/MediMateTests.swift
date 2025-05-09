2836928b2b8e31e8bf71eb1cadab3c314aa05499
Simon Bestler <68077359+bestler@users.noreply.github.com>
2025-05-09 23:30:55 +0200

    Fix git submodule issue

---
+++ app/MediMate/MediMateTests/MediMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  MediMateTests.swift
+//  MediMateTests
+//
+//  Created by Simon Bestler on 09.05.25.
+//
+
+import Testing
+@testable import MediMate
+
+struct MediMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
