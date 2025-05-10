d12285c3793db01efdd46774f55b62d6388ac1e9
JonasM <jonas.maurer@uni-a.de>
2025-05-10 13:38:11 +0200

    ignore google cloud config

--- ai_thing/sts_agent.py
+++ ai_thing/sts_agent.py
@@ -37,9 +37,9 @@ class Assistant(Agent):
             instructions="You are a voice assistant created by LiveKit. Your interface with users will be voice. "
             "You should use short and concise responses, and avoiding usage of unpronouncable punctuation. "
             "You were created as a demo to showcase the capabilities of LiveKit's agents framework.",
-            stt=deepgram.STT(),
+            stt=google.STT(), # gemini
             llm=openai.LLM(model="gpt-4o-mini"),
-            tts=cartesia.TTS(),
+            tts=cartesia.TTS(), # gemini
             # use LiveKit's transformer-based turn detector
             turn_detection=MultilingualModel(),
         )
